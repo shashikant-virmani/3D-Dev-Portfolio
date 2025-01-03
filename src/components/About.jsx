6e5a96969ba16f344f27fb2fdfd44ba37b247d99
Shashikant <kants52167@gmail.com>
2025-01-03 20:31:39 +0530

    Hero Section and About Section done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
+import { Tilt } from 'react-tilt'
+import { motion } from 'framer-motion'
 
-const About = () => {
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+
+const ServiceCard = ({ index, title, icon }) => {
   return (
-    <div>About</div>
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card' 
+      >
+        <div options={{ max: 45, scale: 1, speed: 450 }}
+          className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+        >
+          <img src={icon} alt={title} className='w-16 h-16 object-contain' />
+          <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+        </div>
+      </motion.div>
+    </Tilt>
   )
 }
 
-export default About
+const About = () => {
+  return (
+   <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>Introduction</p>
+      <h2 className={styles.sectionHeadText}>Overview.</h2>
+    </motion.div>
+
+    <motion.p variants={fadeIn("", "", 0.1, 1)} className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
+    Shashikant Virmani is a skilled candidate holding an MCA from NIT Raipur with hands-on product development experience in both frontend (Angular) and backend (Java, Spring Boot) projects in the banking sector. His competitive programming success, including high ranks in Google Kick Start and advanced ratings on CodeChef and Hackerrank, reflects his solid grasp of algorithms and data structures, essential for creating efficient, maintainable code. Shashikant has strong foundations in object-oriented programming, design patterns, and agile workflows, and his project experience includes REST API development. With a proactive learning approach and a collaborative mindset, he is well-equipped to contribute to employer's technical growth, participate in design discussions, and uphold industry coding standards, making him an excellent fit for Software Engineer roles.
+    </motion.p>
+
+    <div className='mt-20 flex flex-wrap gap-10'>
+      {services.map((service, index) => (
+        <ServiceCard key={service.title} index={index} {...service} />
+      ))}
+    </div>
+   </>
+  )
+}
+
+export default SectionWrapper(About, "about")
